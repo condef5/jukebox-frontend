e727c53f81ea2e83823d275e8885e2e14887470e
condef5 <davis.con.fab@gmail.com>
2018-10-15 18:28:41 -0500

    Add apollo to app

---
+++ src/components/Manager.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+/* eslint-disable */
+export class Manager extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      
+    )
+  }
+}
+
+export default {};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { withClientState } from 'apollo-link-state';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+import { GET_SELECTED_REPOSITORIES } from './App';
 import App from './components/App';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cache = new InMemoryCache();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+const GITHUB_BASE_URL = 'https://api.github.com/graphql';
+
+const httpLink = new HttpLink({
+  uri: GITHUB_BASE_URL,
+  headers: {
+    authorization: `Bearer ${'57ba2b4daeb5431fa7af24e4bea9bfc9155e98ab'}`
+  }
+});
+
+const initialState = {
+  selectedRepositoryIds: []
+};
+
+const toggleSelectRepository = (_, { id, isSelected }, { cache }) => {
+  let { selectedRepositoryIds } = cache.readQuery({
+    query: GET_SELECTED_REPOSITORIES
+  });
+
+  selectedRepositoryIds = isSelected
+    ? selectedRepositoryIds.filter(itemId => itemId !== id)
+    : selectedRepositoryIds.concat(id);
+
+  cache.writeQuery({
+    query: GET_SELECTED_REPOSITORIES,
+    data: { selectedRepositoryIds }
+  });
+
+  return { id, isSelected: !isSelected };
+};
+
+const stateLink = withClientState({
+  cache,
+  defaults: initialState,
+  resolvers: {
+    Mutation: {
+      toggleSelectRepository
+    }
+  }
+});
+
+const link = ApolloLink.from([stateLink, httpLink]);
+
+const client = new ApolloClient({
+  link,
+  cache
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
+
 serviceWorker.unregister();
