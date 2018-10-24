34ac74ae600266f0d30d1ac10993cef5146d7ee0
condef5 <davis.con.fab@gmail.com>
2018-10-23 23:55:08 -0500

    Move reproductor render to index.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { hot } from 'react-hot-loader';
 import GenderListContainer from '../containers/GenderListContainer';
 import SingerListContainer from '../containers/SingerListContainer';
@@ -11,52 +11,34 @@ import ButtonList from './ButtonList';
 import VideoPreview from './VideoPreview';
 import Options from './Options';
 import Beat from './Beat';
-import Reproductor from './Reproductor';
 import Manager from './Manager';
 
 import '../App.css';
 
-/* eslint-disable */
-class App extends Component {
-  state = {
-    isReproductor: false
-  };
-  componentDidMount() {
-    const isReproductor = location.href.indexOf('?reproductor') !== -1;
-    this.setState({ isReproductor });
-  }
-
-  render() {
-    const { isReproductor } = this.state;
-    if (isReproductor) {
-      return <Reproductor />;
-    }
-    return (
-      <Manager>
-        <Container>
-          <Row>
-            <Column span="25">
-              {/* Logo */}
-              <Title>Perumatic</Title>
-              <VideoclipListContainer />
-              <ButtonList />
-            </Column>
-            <Column span="50">
-              <Banner />
-              <SingerListContainer />
-              <GenderListContainer />
-              <Options />
-            </Column>
-            <Column span="25">
-              <Controls />
-              <VideoPreview />
-              <Beat />
-            </Column>
-          </Row>
-        </Container>
-      </Manager>
-    );
-  }
-}
+const App = () => (
+  <Manager>
+    <Container>
+      <Row>
+        <Column span="25">
+          {/* Logo */}
+          <Title>Perumatic</Title>
+          <VideoclipListContainer />
+          <ButtonList />
+        </Column>
+        <Column span="50">
+          <Banner />
+          <SingerListContainer />
+          <GenderListContainer />
+          <Options />
+        </Column>
+        <Column span="25">
+          <Controls />
+          <VideoPreview />
+          <Beat />
+        </Column>
+      </Row>
+    </Container>
+  </Manager>
+);
 
 export default hot(module)(App);
--- src/index.js
+++ src/index.js
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
 import { client } from './apollo';
 import App from './components/App';
+import Reproductor from './components/Reproductor';
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById('root')
-);
+// eslint-disable-next-line
+const isReproductor = location.href.indexOf('?reproductor') !== -1;
+
+if (isReproductor) {
+  ReactDOM.render(<Reproductor />, document.getElementById('root'));
+} else {
+  ReactDOM.render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
+    document.getElementById('root')
+  );
+}
 
 serviceWorker.unregister();
