440e5d5fb387d2130e252ebd2da0c4abde183189
condef5 <davis.con.fab@gmail.com>
2018-12-14 10:08:20 -0500

    Add banner for publicity

--- src/components/styles/Banner.js
+++ src/components/styles/Banner.js
@@ -1,23 +1,28 @@
 import styled from 'styled-components';
 
 const Banner = styled.div`
-  padding: 0.5em;
-  display: flex;
-  border-radius: 4px;
-  justify-content: center;
-  align-items: center;
-  box-shadow: 2px 2px 5px #ff0000, -2px -2px 5px #ff0000, -2px 2px 5px #ff0000,
-    2px -2px 5px #ff0000;
-  h3 {
-    text-transform: uppercase;
-    text-align: center;
-    font-size: 48px;
-    margin: 0;
-    -webkit-text-stroke: 1px #ef1a1c;
-    letter-spacing: 3px;
-    color: #ffe100;
-    text-shadow: 3px 3px 0 #ef1a1c, -1px -1px 0 #ef1a1c, 1px -1px 0 #ef1a1c,
-      -1px 1px 0 #ef1a1c, 1px 1px 0 #ef1a1c;
+  max-width: 100%;
+  margin: 0.5em 0;
+
+  img {
+    display: none;
+  }
+
+  .row {
+    height: 150px;
+  }
+
+  .swiper-slide {
+    padding: 0.5em;
+  }
+
+  .wrap {
+    background-size: cover;
+    width: 100%;
+    height: 100%;
+    border-radius: 5px;
+    box-shadow: rgb(255, 0, 0) 2px 2px 5px, rgb(255, 0, 0) -2px -2px 5px,
+      rgb(255, 0, 0) -2px 2px 5px, rgb(255, 0, 0) 2px -2px 5px;
   }
 `;
 
