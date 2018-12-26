82d8934fe08be50a22955c05e46b8a849a0c327f
condef5 <davis.con.fab@gmail.com>
2018-12-26 10:24:14 -0500

    Add Endpoint production

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 export const endpoint = `http://localhost:4000/graphql`;
-export const prodEndpoint = `https://sickfits-yoga-prod.herokuapp.com/`;
+export const prodEndpoint = `http://backend-perumatic.tk/graphql`;
